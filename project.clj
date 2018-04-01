0b385dd3049fe5c9e56a10d57f4e2d389c5424f4
Henrik Heine <henrikheine@web.de>
2018-04-01 10:04:20 +0000

    dependencied

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject solo "0.1.0-SNAPSHOT"
   :source-paths ["src/clj"]
-  :resource-paths ["lib/clojure-1.8.0.jar" "lib/log4j-1.2.17.jar"])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [log4j/log4j "1.2.17"]])
 
