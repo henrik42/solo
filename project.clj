d60ba7cb75c25f08d62893285fabfad50dcf3988
Henrik Heine <henrikheine@web.de>
2018-04-02 19:38:15 +0000

    step six

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject solo "0.1.0-SNAPSHOT"
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [swank-clojure/swank-clojure "1.4.3"]
                  [log4j/log4j "1.2.17"]])
-
