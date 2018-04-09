32347d6e9041f0e0d3484aaedba0ddba7a687f04
Henrik Heine <henrikheine@web.de>
2018-04-09 07:40:21 +0000

    make-jumpstart-jsf added

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [swank-clojure/swank-clojure "1.4.3"]
                  [log4j/log4j "1.2.17"]]
-  :plugins [[lein-swank "1.4.5"]])
+  :plugins [[lein-swank "1.4.5"]]
+  :aliases {"make-jumpstart-jsf" ["with-profile" "jumpstart-jsf" "do" ["clean"] "jar"]}
+  :profiles {:jumpstart-jsf {:resource-paths ^:replace ["jumpstart/resources"]
+                             :aot :all
+                             :main solo.jumpstart.jsf
+                             :source-paths ^:replace ["jumpstart/src"]}})
