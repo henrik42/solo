4fe1f6494896398f1803949c630a3add3dd43b6c
Henrik Heine <henrikheine@web.de>
2018-05-14 20:18:03 +0000

    make-web-war

--- project.clj
+++ project.clj
@@ -12,32 +12,32 @@
             [lein-ring "0.12.4"]
             [lein-codox "0.10.3"]]
   
-  :aliases {"make-jumpstart" ["with-profile" "make-jumpstart" "do" ["clean"] "jar"]
+  :aliases {"make-jumpstart" ["with-profile" "+make-jumpstart" "do" ["clean"] "jar"]
             
             ;; generate HTML documentation into
             ;; resources/public/generated-doc/. This will be included
             ;; in JARs/WARs and can be served to end users at runtime
             ;; through solo.web/app --
             ;; http://localhost:3000/generated-doc/index.html
-            "make-doc" ["with-profile" "make-doc" "do" ["clean"] ["codox"]]
+            "make-doc" ["with-profile" "+make-doc" "do" ["clean"] ["codox"]]
             
             ;; deployable WAR that can be deployed side-by-side with
             ;; JEE Host Application. Contains Solo Web-App & Core &
-            ;; nREPL Server.
-            "make-web-war" ["with-profile" "make-web-war" "ring" "uberwar" "solo-web.war"]
+            ;; nREPL Server (not log4j JAR!)
+            "make-web-war" ["with-profile" "+make-web-war" "ring" "uberwar" "solo-web.war"]
 
             ;; executable uber JAR that runs Jetty with Solo Web-App
             ;; and optionally connects to remotely running nREPL
             ;; server with Solo Core.
-            "make-web-jar" ["with-profile" "make-web-jar" "do" ["clean"] ["uberjar"]]
+            "make-web-jar" ["with-profile" "+make-web-jar" "do" ["clean"] ["uberjar"]]
 
-            "make-module-jar" ["with-profile" "make-module-jar" "do" ["clean"] ["uberjar"]]
+            "make-module-jar" ["with-profile" "+make-module-jar" "do" ["clean"] ["uberjar"]]
 
             ;; just run solo.webapp/-main. Note: lein ring server-headless
             ;; just starts Jetty and uses solo.web/app as the handler. solo.webapp/-main
             ;; does a lot more. Usage:
             ;; lein run-web-jar -j 3000 
-            "run-web-jar" ["with-profile" "make-web-jar" "trampoline" "run"]}
+            "run-web-jar" ["with-profile" "+make-web-jar" "trampoline" "run"]}
   
   :ring {:handler solo.web/app
          :nrepl {:start? true
@@ -53,10 +53,13 @@
                                   [hiccup "1.0.5"]
                                   [robert/hooke "1.3.0"]
                                   [org.clojure/tools.cli "0.3.7"]]}
+             
              :make-doc {:clean-targets ^{:protect false} ["resources/public/generated-doc"]}
-             :make-module-jar {:--exclusions [log4j/log4j]
-                               :--uberjar-exclusions [#"org/apache/log4j/"]}
-             :make-web-jar {:main solo.webapp
+             
+             :make-module-jar {}
+             
+             :make-web-jar {:main solo.main
+                            :aot [solo.main]
                             :dependencies [[log4j/log4j "1.2.17"]
                                            [ring/ring-core "1.6.3"]
                                            [ring/ring-jetty-adapter "1.6.3"]
@@ -64,6 +67,9 @@
                                            [hiccup "1.0.5"]
                                            [robert/hooke "1.3.0"]
                                            [org.clojure/tools.cli "0.3.7"]]}
+
+             ;; :provided {:dependencies [[log4j/log4j "1.2.17"]]}
+             
              :make-web-war {:ring {:handler solo.webapp/app
                                    :init    solo.webapp/init
                                    :destroy solo.webapp/destroy}
@@ -74,6 +80,7 @@
                                            [hiccup "1.0.5"]
                                            [robert/hooke "1.3.0"]
                                            [org.clojure/tools.cli "0.3.7"]]}
+             
              :make-jumpstart {:resource-paths ^:replace ["jumpstart/resources"]
                               :aot :all
                               :main solo.jumpstart.servlet_container_initializer
--- src/clj/solo/webapp.clj
+++ src/clj/solo/webapp.clj
@@ -14,14 +14,9 @@
   running nREPL-server which then calls `solo.core` for accessing the
   log4j API."
   
-  (:gen-class)
   (:require [solo.nrepl :as nrepl]
-            [solo.jetty :as jetty]
-            [solo.swank :as swank]
-            [solo.web :as web]
-            [ring.util.response :as response]
-            [robert.hooke :as hooke]
-            [clojure.tools.cli :refer [parse-opts]]))
+            ;;[solo.web :as web]
+            [ring.util.response :as response]))
 
 ;; ring.util.response/resource-data does not know how to handle Apache
 ;; VFS URLs which we find in JBoss/Wildfly.
@@ -36,7 +31,7 @@
 (def app
   "An alias for `solo.web/app`."
 
-  web/app)
+  (atom nil))
 
 (def nrepl-server
   "Atom that holds an nREPL-server (see `init`)."
@@ -50,6 +45,8 @@
   already opened. So we will just keep on going in this case."
 
   []
+  (require 'solo.web)
+  (def app (eval (read-string "solo.web/app")))
   (try 
     (reset! nrepl-server
             (nrepl/start-server :port 7888))
@@ -67,90 +64,3 @@
     (reset! nrepl-server nil)
     (.close server)))
 
-;; ---------------------------- main ---------------------------------
-
-(defn remote-wrapper
-  "Returns a function that delegates calls to an nREPL-server.
-
-  On invocation of the returned function `conn-fn` is (repeatedly)
-  used as a factory for an nREPL connection. `solo.nrepl/remote-eval`
-  is then used to evaluate the invocation of `(apply @fn-var args)` on
-  the remote server. Returns the result or throws exception."
-
-  [conn-fn fn-var]
-  (fn [_ & args]
-    (nrepl/remote-eval
-     (conn-fn)
-     (format "(apply %s/%s [%s])"
-             (-> fn-var meta :ns) (-> fn-var meta :name)
-             (apply pr-str args)))))
-
-(defn parse-host:port
-  "Parses string argument `[<host>:]<port>`. Returns map with `:host`
-  and `:port` for non-nil key-values (plays well with `{:keys [port
-  host] :or [host <host-default> port <port-default>]}`
-  destructuring)."
-
-  [host:port]
-  (let [[_ host port] (re-matches #"([^:]+)??:?([0-9]+)" host:port)
-        res (if host {:host host} {})]
-    (if port (assoc res :port (Integer/parseInt port)) res)))
-
-(def cli-options
-  "CLI options."
-  
-  [["-r" "--remote-core [<host>:]<port>" "intercept/delegate calls to solo.core to remote nREPL server)"
-    :parse-fn #(parse-host:port %)]
-   ["-n" "--nrepl [<host>:]<port>" "start nREPL server on <host>:<port> (<host> defaults to \"0.0.0.0\")"
-    :parse-fn #(parse-host:port %)]
-   ["-j" "--jetty [<host>:]<port>" "start Jetty server on <host>:<port>"
-    :parse-fn #(parse-host:port %)]
-   ["-s" "--swank [<host>:]<port>" "start Swank server on <host>:<port>"
-    :parse-fn #(parse-host:port %)]])
-
-(defn usage
-  "Prints usage info and `(System/exit 1)` exits the JVM."
-
-  [msg summary]
-  (.println System/out (format "USAGE: [options]\n%s\n%s" msg summary))
-  (System/exit 1))
-
-(defn -main
-  "Starts a Jetty server (and optionally nREPL & Swank) with
-  `solo.web/app`."
-
-  [& args]
-  (let [{:keys [errors options arguments summary] :as opt} (parse-opts args cli-options)
-        _ (when errors (usage errors summary))
-        {:keys [remote-core nrepl jetty swank]} options
-        
-        {jetty-port :port jetty-host :host :or {jetty-host "0.0.0.0"}} jetty
-        _ (when-not jetty-port (usage "Invalid Jetty option" summary))
-        
-        {nrepl-port :port nrepl-host :host :or {nrepl-host "0.0.0.0"}} nrepl
-        _ (when (and nrepl (not nrepl-port)) (usage "Invalid nREPL option" summary))
-        
-        {swank-port :port swank-host :host :or {swank-host "0.0.0.0"}} swank
-        _ (when (and swank (not swank-port)) (usage "Invalid Swank option" summary))
-        
-        {remote-core-port :port remote-core-host :host :or {remote-core-host "127.0.0.1"}} remote-core
-        _ (when (and remote-core (not remote-core-port)) (usage "Invalid remote-core option" summary))
-
-        _ (when (seq arguments) (usage (str "Unexpected arguments" arguments) summary))
-        
-        conn-fn #(nrepl/get-connection {:port remote-core-port :host remote-core-host})]
-    
-    (when remote-core
-      (hooke/add-hook #'solo.core/get-logger
-                      (remote-wrapper conn-fn #'solo.core/get-logger))
-      (hooke/add-hook #'solo.core/set-log-level!
-                      (remote-wrapper conn-fn #'solo.core/set-log-level!))
-      (hooke/add-hook #'solo.core/get-current-loggers
-                      (remote-wrapper conn-fn #'solo.core/get-current-loggers)))
-    
-    (when nrepl-port (nrepl/start-server :port nrepl-port :host nrepl-host))
-    (when swank-port (swank/start-server :port swank-port :host swank-host))
-
-    (jetty/start-server :port jetty-port :host jetty-host)
-    
-    (.println System/out "CTRL-C to quit...")))
