0051ba3219da1b09c57a729dbaf5cf09c7ad96d6
Henrik Heine <henrikheine@web.de>
2018-05-27 07:52:35 +0000

    .

--- scripts/make-jumpstart-jsf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-mkdir -p classes/
-rm -rf classes/*
-java -cp jumpstart/src/:classes/:lib/clojure-1.8.0.jar clojure.main -e "(compile 'solo.jumpstart.jsf)"
-jar -vcf jumpstart-jsf.jar -C classes/ solo
-jar -vuf jumpstart-jsf.jar -C jumpstart/resources/ META-INF
