a1680d4e9f8a46e0d9cfa743f052ca1d7017db4b
Henrik Heine <henrikheine@web.de>
2018-05-27 09:13:30 +0000

    .

--- src/clj/solo/jumpstart.clj
+++ src/clj/solo/jumpstart.clj
@@ -1,8 +1,23 @@
 (ns solo.jumpstart
-  (:require [solo.swank :as swank]))
-    
-(defn jumpstart []
-  (println "solo.jumpstart/jumpstart: starting up:")
-  (swank/start-server)
-  (println "solo.jumpstart/jumpstart: done."))
+  "Namespace that will/can be loaded to jump-start server-side code.
 
+  See `solo.jumpstart.servlet_container_initializer`.
+
+  **Note:** This namespaces requires `solo.core` so that after being
+    jump-started you may use `solo.core` via nREPL without the need to
+    require/load `solo.core` explicitly."
+  (:require [solo.nrepl :as nrepl]
+            [solo.core]))
+
+(defn jumpstart
+  "Will/can be called from
+  `solo.jumpstart.servlet_container_initializer/-onStartup`.
+
+  Can be over-written/redefined in runtime/production
+  environments. This implementation just
+  calls `(solo.nrepl/start-server)`."
+
+  []
+  (println "solo.jumpstart/jumpstart: starting up:")
+  (nrepl/start-server)
+  (println "solo.jumpstart/jumpstart: done."))
