50f5212828bd0fc9307ebe1773825ac21c245dea
Henrik Heine <henrikheine@web.de>
2018-05-27 08:35:54 +0000

    .

--- src/clj/solo/core.clj
+++ src/clj/solo/core.clj
@@ -1,13 +1,14 @@
 (ns solo.core
   "Core business logic.
 
-  Function for accessing log4j loggers."
+  Functions for accessing log4j loggers."
   (:import [org.apache.log4j Logger Level]))
 
 (defn logger->map
   "Returns a map `{:logger-name <String:logger-name> :log-level
-   <String:log-level>}` for the given non-nil log4j `Logger`. If the
-   logger has no level set, `:log-level` will be `\"\"`."
+   <String:log-level>}` for the given non-`nil` log4j `Logger`. If the
+   logger has no level set, `:log-level` will be `\"\"` (empty
+   `String`)."
 
   [log4j-logger]
   {:pre [(not (nil? log4j-logger))]}
@@ -15,22 +16,37 @@
    :log-level (-> log4j-logger .getLevel str)})
 
 (defn get-logger
-  "Returns the log4j Logger with the given `logger-name` as a
-  map (`logger->map`). Never returns `nil`."
+  "Returns the log4j logger (as of `logger->map`) with the given
+  non-`nil` `<String:logger-name>`. Never returns `nil`. Passing
+  `\"root\"` returns the root logger. "
 
   [logger-name]
-  (-> (Logger/getLogger logger-name) logger->map))
+  {:pre [(string? logger-name)]}
+  (-> (if (= "root" logger-name)
+        (Logger/getRootLogger)
+        (Logger/getLogger logger-name))
+      logger->map))
 
-(defn set-log-level! [logger-name log-level]
-  (-> (Logger/getLogger logger-name) (.setLevel (Level/toLevel log-level))))
+(defn set-log-level!
+  "Sets the logger's `<String:log-level>`. Passing `\"root\"` as
+  `<String:logger-name>` will set the root logger's log-level."
+
+  [logger-name log-level]
+  {:pre [(string? logger-name)]}
+  (-> (if (= "root" logger-name)
+        (Logger/getRootLogger)
+        (Logger/getLogger logger-name))
+      (.setLevel (Level/toLevel log-level))))
 
 (defn get-current-loggers
-  "Returns a seq of the current log4 loggers as of `logger->map`."
+  "Returns a map-seq (as of `logger->map`) of the current log4
+  loggers (incl. the root logger)."
   
   []
   (map logger->map
-       (-> (Logger/getRootLogger)
-           .getLoggerRepository
-           .getCurrentLoggers
-           enumeration-seq)))
-
+       (conj
+        (-> (Logger/getRootLogger)
+            .getLoggerRepository
+            .getCurrentLoggers
+            enumeration-seq)
+        (Logger/getRootLogger))))
