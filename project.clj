6890e972ddb4783818dfe83e5f701354aa01577a
Henrik Heine <henrikheine@web.de>
2018-05-27 09:40:05 +0000

    .

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
                  [org.clojure/tools.nrepl "0.2.12"]]
   
   :plugins [[lein-swank "1.4.5"]
-            [lein-ring "0.12.4"]
             [lein-codox "0.10.3"]
             [lein-marginalia "0.9.1"]]
   
@@ -31,12 +30,6 @@
                          "src/clj"
                          "jumpstart/src"]]
 
-            ;; build WAR that can be deployed side-by-side with JEE
-            ;; host application. Contains Solo Web-App & Core & nREPL
-            ;; Server (not log4j JAR -- which must come with host
-            ;; application)
-            "make-web-war" ["with-profile" "+make-web-war,+web-deps" "ring" "uberwar" "solo-web.war"]
-
             ;; build JAR that contains Solo's backend --
             ;; i.e. `solo.core`, `solo.repl` and their dependencies
             ;; (incl. Clojure JAR but not log4j JAR -- which must come
@@ -51,56 +44,20 @@
             ;; to load/call (i.e. jump-start)
             ;; `solo.jumpstart/jumpstart` (see make-module-jar) when
             ;; host/web application starts.
-            "make-jumpstart" ["with-profile" "+make-jumpstart" "do" ["clean"] "jar"]
-            
-            ;; executable uber JAR that runs Jetty with Solo Web-App
-            ;; and optionally connects to remotely running nREPL
-            ;; server with Solo Core.
-            "make-web-jar" ["with-profile" "+make-web-jar,+web-deps" "do" ["clean"] ["uberjar"]]
-
-            ;; just run solo.main/-main. Note: lein ring
-            ;; server-headless just starts Jetty and uses solo.web/app
-            ;; as the handler. solo.main/-main does a lot more. Usage:
-            ;; lein run-web-jar -j 3000
-            "run-web-jar" ["with-profile" "+make-web-jar" "trampoline" "run"]}
-  
-  :ring {:handler solo.web/app
-         :nrepl {:start? true
-                 :port 9998}}
+            "make-jumpstart" ["with-profile" "+make-jumpstart" "do" ["clean"] "jar"]}
   
   :codox {:metadata {:doc/format :markdown}
           :doc-files ["doc/intro.md", "README.md"]
           :output-path "resources/public/generated-doc/"}
   
-  :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]
-                                  [ring/ring-core "1.6.3"]
-                                  [ring/ring-jetty-adapter "1.6.3"]
-                                  [compojure "1.6.0"]
-                                  [hiccup "1.0.5"]
-                                  [robert/hooke "1.3.0"]
-                                  [org.clojure/tools.cli "0.3.7"]]}
+  :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}
 
-             :provided {:dependencies [[ring/ring-jetty-adapter "1.6.3"]]}
-             
-             :web-deps {:dependencies [[ring/ring-core "1.6.3"]
-                                       ;;[ring/ring-jetty-adapter "1.6.3"]
-                                       [compojure "1.6.0"]
-                                       [hiccup "1.0.5"]
-                                       [robert/hooke "1.3.0"]
-                                       [org.clojure/tools.cli "0.3.7"]]}
+             :provided {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]}
              
              :make-doc {:source-paths ["jumpstart/src"]
                         :clean-targets ^{:protect false} ["resources/public/generated-doc"]}
              
              :make-module-jar {}
-             
-             :make-web-jar {:main solo.main
-                            :aot [solo.main]
-                            :dependencies [[log4j/log4j "1.2.17"]]}
-
-             :make-web-war {:ring {:handler solo.webapp/app
-                                   :init    solo.webapp/init
-                                   :destroy solo.webapp/destroy}}
              
              :make-jumpstart {:resource-paths ^:replace ["jumpstart/resources"]
                               :aot [solo.jumpstart.servlet_container_initializer]
