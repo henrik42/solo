dbc4d75fabef3af85ea2e2f8e3cfe3ecd9e92a00
Henrik Heine <henrikheine@web.de>
2018-05-27 08:12:12 +0000

    .

--- src/clj/solo/swank.clj
+++ src/clj/solo/swank.clj
@@ -1,7 +1,43 @@
 (ns solo.swank
+  "Functions around Swank."
+  
   (require [swank.swank :as swank]))
 
-(defn -main [& args]
+(defn start-server
+  "Starts a Swank server on given port/host. There can be atmost one
+  running server at any time. A running server may be shut down
+  by `(stop-server)`. Throws exception if the server cannot be
+  started. Returns the server.
+
+  Example:
+
+      (start-server :port 2001 :host \"127.0.0.1\")"
+
+  [& {:keys [port host]
+      :or {port 4005 host "0.0.0.0"}}]
+  (let [params {:port port :host host}
+        _ (.println System/out (str "Starting Swank server on " params " ..."))
+        server (try
+                 (swank/start-server :port port :host host)
+                 (catch Throwable t
+                   (throw (ex-info
+                           (format "Could not start Swank server: %s Cause: %s" params t)
+                           params t))))]
+    (.println System/out (str "Started Swank server on " params "."))
+    server))
+
+(defn stop-server
+  "Stops a running Swank server."
+
+  []
+  (swank/stop-server))
+  
+(defn -main
+  "Main entry point for starting a Swank server.
+
+  This function is just for testing (not a full CLI)."
+
+  [& args]
   (let [{:keys [host port] :or {host "0.0.0.0" port 4005}}
         (map (comp read-string str) args)]
-    (swank/start-server :port port :host host)))
+    (start-server :port port :host host)))
