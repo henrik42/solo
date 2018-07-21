29a0126dd9ef804ac0c687064221271cebdd8455
Henrik Heine <henrikheine@web.de>
2018-07-21 07:55:26 +0000

    slideshow

--- src/cljs/solo/talk.cljs
+++ src/cljs/solo/talk.cljs
@@ -1,7 +1,7 @@
 (ns solo.talk
   (:require [reagent.core :as r]))
 
-(defonce state (r/atom {}))
+(defonce state (r/atom {:slide 1}))
 
 (defn show-state []
   [:div (str @state)])
@@ -11,11 +11,41 @@
 
 (defn enter-something []
   [:input {:type "text"
+           :default-value (:name @state)
            :on-change #(swap! state assoc :name (-> % .-target .-value))}])
+
+(defn seiten-wahl []
+  [:select {:value (:slide @state)
+            :on-change #(swap! state assoc :slide (-> % .-target .-value js/parseInt))}
+   (for [i [1 2 3]]
+     [:option i])])
+
+(defmulti slide identity)
+
+(defmethod slide 1 []
+  [:span
+   [:div.title.boxed "slide 1:"]
+   [:div
+    "Dies ist eine interaktve Seite mit einem Textfeld :"
+    [enter-something]]])
+
+(defmethod slide 2 []
+  [:div.title "slide 2:"])
+
+(defmethod slide 3 []
+  [:span "slide 3"])
+
+(defmethod slide :default []
+  [:span "oops"])
+
+(defn current-slide []
+  (slide (:slide @state)))
 
 (r/render [:div
            [show-state]
-           [show-something "foo"]
-           [enter-something]]
+           [seiten-wahl]
+           [current-slide]
+           #_ [show-something "foo"]
+           #_ [enter-something]]
           (js/document.getElementById "main"))
 
--- src/cljs/solo/talk.md
+++ src/cljs/solo/talk.md
@@ -82,8 +82,41 @@
               (js/document.getElementById "main"))
     
 
+# (9) Slideshow
 
-
+    (defn seiten-wahl []
+      [:select {:value (:slide @state)
+                :on-change #(swap! state assoc :slide (-> % .-target .-value js/parseInt))}
+       (for [i [1 2 3]]
+         [:option i])])
+    
+    (defmulti slide identity)
+    
+    (defmethod slide 1 []
+      [:span
+       [:div.title.boxed "slide 1:"]
+       [:div
+        "Dies ist eine interaktve Seite mit einem Textfeld :"
+        [enter-something]]])
+    
+    (defmethod slide 2 []
+      [:div.title "slide 2:"])
+    
+    (defmethod slide 3 []
+      [:span "slide 3"])
+    
+    (defmethod slide :default []
+      [:span "oops"])
+    
+    (defn current-slide []
+      (slide (:slide @state)))
+    
+    (r/render [:div
+               [show-state]
+               [seiten-wahl]
+               [current-slide]
+              (js/document.getElementById "main"))
+    
 # (2) Namespace Angaben
 
     (:require-macros [cljs.core.async.macros :refer [go]])
